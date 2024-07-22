ea5ce52bc2cda4e042491790c14643ed02956ee2
danisebas4 <92006767+danisebas4@users.noreply.github.com>
2024-07-21 22:19:14 -0400

    MovimientoAutoObstaculo
    Movimiento de auto obstaculo randomico, pero aún brusco.

--- assets/miniJuegoPersecucion/Scripts/auto_movil2.gd
+++ assets/miniJuegoPersecucion/Scripts/auto_movil2.gd
@@ -1,14 +1,27 @@
 extends CharacterBody2D
 
 
-@export var SPEED = 200.0
+@export var SPEED = 200.0;
+@export var FRICCION=0.5;
 
 
+#vector2d para almacenar las direcciones
+var axis : Vector2;
+			
+func entradasDetectadasPorBoleano() -> Vector2:
+	axis.x=int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"));
+	axis.y=int(Input.is_action_pressed("ui_up"))-int(Input.is_action_pressed("ui_down"));
+	return axis.normalized();
 
+
+func movimientoPorEntradasBooleanas() -> void:
+	velocity.x=entradasDetectadasPorBoleano().x*SPEED;
+	velocity.y=entradasDetectadasPorBoleano().y*-SPEED;
+	move_and_slide();
+	
+	
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-
-func _physics_process(delta):
-
+func _movimientoPorTeclasPreConfiguradas():
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 
@@ -27,3 +40,8 @@ func _physics_process(delta):
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
+
+
+func _physics_process(delta):
+	movimientoPorEntradasBooleanas();
+
