ff84f66aec8a128df1c22726541126e397085dd8
santiagobalcazar <santiago.balcazar3@gmail.com>
2024-08-02 11:54:07 -0400

    ordeando carpetas del minijuego 1

--- scripts/comisaría.gd
+++ scripts/comisaría.gd
@@ -1,15 +1,2 @@
 extends StaticBody2D
 
-
-#script para desvanecer edificios cuando pasas por detras
-func _on_area_2d_body_entered(body):
-	if body is player:
-		modulate.a = 0.5
-		print("entro al area a desvanecer")
-
-
-
-func _on_area_2d_body_exited(body):
-	if body is player:
-		modulate.a = 255
-		print("salio del area a desvanecer")
