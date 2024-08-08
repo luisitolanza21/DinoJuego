8146374ea1b6ca67ce039a4e7c71808971a742be
danisebas4 <92006767+danisebas4@users.noreply.github.com>
2024-08-07 22:36:10 -0400

    Autos Aleatorios
    Implementaciòn de autos aleatorios en persecuciòn

--- scripts/minijuegoPersecusion/dialogosPersecusion.gd
+++ scripts/minijuegoPersecusion/dialogosPersecusion.gd
@@ -1,6 +1,7 @@
 extends Control
 
-var dialog = ["asdkakjdasjdsakj"]
+
+var dialog = ["PERSIGUE AL AUTOMÓVIL SOSPECHOSO!!!"]
 var dialog_index = 0
 var finish = false
 var text_speed = 0.1
@@ -20,12 +21,14 @@ func set_dialog_text(new_dialog: Array):
 func load_dialog():
 	if dialog_index < dialog.size():
 		finish = false
-		$RichTextLabel.bbcode_text = dialog[dialog_index]
-		$RichTextLabel.visible_characters = 0
+		
+		$labelTexto.bbcode_text = dialog[dialog_index]
+		$labelTexto.visible_characters = 0
+	
 		var tween_duration = text_speed * dialog[dialog_index].length()
 		current_tween = get_tree().create_tween()
 		is_typing = true
-		current_tween.tween_property($RichTextLabel, "visible_characters", dialog[dialog_index].length(), tween_duration)
+		current_tween.tween_property($labelTexto, "visible_characters", dialog[dialog_index].length(), tween_duration)
 		current_tween.finished.connect(self._on_tween_finished)
 	else:
 		queue_free()
---
+++ scripts/minijuegoPersecusion/obstaculosAleatorios.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+var autoObstaculo=load("res://scenes/minijuegoPersecusion/autoObstaculo.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if $TiempoGen.is_stopped():
+		create_obstacles();
+		$TiempoGen.start();
+		
+func create_obstacles():
+	# Instanciar la escena del auto obstáculo
+	var new_obs = autoObstaculo.instantiate()
+	if new_obs:
+		print("Nuevo obstáculo instanciado.")
+	# Asignar una posición aleatoria en y
+	var x=$"../DINOMOVIL".positi
+	var y=randf_range(468, 650)
+	new_obs.position = Vector2(0, y);
+	# Añadir el nuevo obstáculo a la escena actual
+	add_child(new_obs)
+	print("Obstáculo añadido a la escena. y="+str(y))
