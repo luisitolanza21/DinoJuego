b1c35b5f4e00f9dbf820080d68fdc17e311e5f65
santiagobalcazar <santiago.balcazar3@gmail.com>
2024-08-10 03:13:33 -0400

    nivel 3 del minijuego 1 medio terminado

---
+++ scripts/minijuego1/npc.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@export var player_name = "player"
+@export var dialogo : String = "res://dialogs/dinodetective1/minijuego1_1.dialogue"
+@export var flag : String = "npc1"
+@export var dialogueStart : String = "Start"
+var listo = Global.listo
+
+func _ready():
+	$Area2D.connect("body_entered", Callable(self, "_on_body_entered"))
+	$Area2D.connect("body_exited", Callable(self, "_on_body_exited"))
+
+
+func _on_body_entered(body):
+	print(listo)
+	if body.name == player_name:
+		body.set("current_npc", self)
+
+func _on_body_exited(body):
+	print(listo)
+	if body.name == player_name:
+		body.set("current_npc", null)
+
+func player_interacted():
+	print("Player interacted with NPC")
+	DialogueManager.show_dialogue_balloon(load(dialogo), flag)
+	
