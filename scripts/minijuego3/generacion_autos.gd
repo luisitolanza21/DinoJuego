2717b4427897b9c8ca0418c37b4aaa52ae5f935e
santiagobalcazar <santiago.balcazar3@gmail.com>
2024-08-10 21:18:40 -0400

    toque una imagen del dino auto de pua sin querer asi que toca hacer un push vacio

---
+++ scripts/minijuego3/generacion_autos.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+var autoObstaculo=load("res://scenes/minijuego3/autos.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if $TiempoGen.is_stopped():
+		create_obstacles();
+		$TiempoGen.start();
+		
+func create_obstacles():
+	# Instanciar la escena del auto obstáculo
+	var new_obs = autoObstaculo.instantiate()
+	if new_obs:
+		print("Nuevo obstáculo instanciado.")
+	# Asignar una posición aleatoria en y
+
+
+	var y= 50
+	new_obs.position = Vector2(-580, y);
+	# Añadir el nuevo obstáculo a la escena actual
+	add_child(new_obs)
+	print("Obstáculo añadido a la escena. y="+str(y))
