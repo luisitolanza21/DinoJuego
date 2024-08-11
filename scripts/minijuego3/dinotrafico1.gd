270736c588dc2a35ce4951ea260063e151e2a1f7
santiagobalcazar <santiago.balcazar3@gmail.com>
2024-08-11 01:44:25 -0400

    dinominijuego 3 medio funciona el nivel 1

--- scripts/minijuego3/dinotrafico1.gd
+++ scripts/minijuego3/dinotrafico1.gd
@@ -1,12 +1,29 @@
 extends Button
 @onready var animated = $animacion
+var MAX_SPEED = 0
 
 func _ready():
 	animated.play("frente")
 
 func _on_pressed():
-	$animacion.flip_h = true
-	if animated.animation == "frente":
-		animated.play("lado")
+	animated.flip_h = true
+	if animated.animation == "frente":	
+		_mostrar_lado()
 	else:
-		animated.play("frente")
+		_mostrar_frente()
+		
+
+func _mostrar_frente():
+	animated.play("frente")
+	MAX_SPEED=randf_range(200, 500);
+	print(MAX_SPEED)
+	$frenoarriba.position = Vector2(0,0)
+	$frenoizquierdo.position = Vector2(0,1200)
+	#$frenoizquierdo.hide()
+
+func _mostrar_lado():
+	animated.play("lado")
+	MAX_SPEED=0;
+	$frenoarriba.position = Vector2(0,1200)
+	$frenoizquierdo.position = Vector2(0, 0)
+	#$frenoizquierdo.show()
--- scripts/minijuego3/generacion_autos.gd
+++ scripts/minijuego3/generacion_autos.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var autoObstaculo=load("res://scenes/minijuego3/autos.tscn")
+var autoObstaculovertical=load("res://scenes/minijuego3/autosvertical.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,13 +18,21 @@ func _process(delta):
 func create_obstacles():
 	# Instanciar la escena del auto obstáculo
 	var new_obs = autoObstaculo.instantiate()
-	if new_obs:
-		print("Nuevo obstáculo instanciado.")
+	var new_obsvertical = autoObstaculovertical.instantiate()
+	#if new_obs:
+	#	print("Nuevo obstáculo instanciado.")
 	# Asignar una posición aleatoria en y
 
 
-	var y= 50
-	new_obs.position = Vector2(-580, y);
+	
+	new_obs.position = Vector2(-780, 70);
 	# Añadir el nuevo obstáculo a la escena actual
 	add_child(new_obs)
-	print("Obstáculo añadido a la escena. y="+str(y))
+	#print("Obstáculo añadido a la escena. y="+str(y))
+	
+	
+	new_obsvertical.position = Vector2(-250, -550);
+	# Añadir el nuevo obstáculo a la escena actual
+	add_child(new_obsvertical)
+	#print("Obstáculo añadido a la escena. y="+str(y))
+
