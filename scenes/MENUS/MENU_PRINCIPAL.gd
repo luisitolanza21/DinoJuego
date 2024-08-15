9a30e1c4fe8c819944b3226bae582d8831207ef0
luisitolanza21 <lanzatordoya@gmail.com>
2024-08-15 14:40:24 -0400

    error _ botones_inicio aun no resuelto

--- scenes/MENUS/MENU_PRINCIPAL.gd
+++ scenes/MENUS/MENU_PRINCIPAL.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
 func _process(delta):
+	Global.botones = false
 	if Input.is_action_just_pressed("ui_accept"):
 		Transicion.cambiar_escena("res://scenes/world.tscn")
--- scripts/WORLD/logica_world.gd
+++ scripts/WORLD/logica_world.gd
@@ -11,8 +11,8 @@ func _ready():
 	# Cargar el recurso de diálogo
 	Misiones.esta_en_world = true # referencia a que volvimos a la escena mundo
 	$player.position = Global.dino_position
-	
 	if Misiones.mision == 0: #introduccion
+		Global.botones = false
 		var dialogue_resource = load(Misiones.get_current_dialogue_path())
 		DialogueManager.show_dialogue_balloon(dialogue_resource)
 		
